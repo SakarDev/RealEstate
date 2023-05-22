a2d4b1f66afb93c8e30c5626e804b17cccfea69d
Sakar <sakar.hamasaid@gmail.com>
2023-05-22 08:02:48 +0300

    fix error <: Unexpected token ..etc

--- includes/conn.php
+++ includes/conn.php
@@ -9,10 +9,3 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error){
 	die('Failed to connect: '.$conn->connect_error);
 }
-// else{
-// 	echo 'connected sucessfuly!';
-// }
-
-?>
-
-
